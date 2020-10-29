7f44cbfd98fdf0f1bbf82310bf171d6acdf16f27
Den Urakolouy <41019056+URAKOLOUY5@users.noreply.github.com>
2020-10-29 17:49:15 +0300

    nh2_sounds_weapon.lua (hatchet)

---
+++ lua/autorun/nh2_sounds_weapon.lua
@@ -0,0 +1,88 @@
+-- weapons\weapon_nh_hatchet.lua
+
+-------------------------------------------------------
+sound.Add( {
+	name = "NH2.Hatchet_Swing1",
+	channel = CHAN_WEAPON,
+	volume = 0.55,
+	level = 105,
+	pitch = 100,
+	sound = "weapons_nh/hatchet/swing1.wav"
+} )
+
+sound.Add( {
+	name = "NH2.Hatchet_Swing2",
+	channel = CHAN_WEAPON,
+	volume = 0.55,
+	level = 105,
+	pitch = 100,
+	sound = "weapons_nh/hatchet/swing2.wav"
+} )
+
+--
+
+sound.Add( {
+	name = "NH2.Hatchet_HeavySwing1",
+	channel = CHAN_WEAPON,
+	volume = 0.55,
+	level = 105,
+	pitch = 100,
+	sound = "weapons_nh/hatchet/heavy_swing1.wav"
+} )
+
+sound.Add( {
+	name = "NH2.Hatchet_HeavySwing2",
+	channel = CHAN_WEAPON,
+	volume = 0.55,
+	level = 105,
+	pitch = 100,
+	sound = "weapons_nh/hatchet/heavy_swing2.wav"
+} )
+
+-------------------------------------------------------
+sound.Add( {
+	name = "NH2.Hatchet_Stab1",
+	channel = CHAN_WEAPON,
+	volume = 0.55,
+	level = 105,
+	pitch = 100,
+	sound = "weapons_nh/hatchet/stab1.wav"
+} )
+
+sound.Add( {
+	name = "NH2.Hatchet_Stab2",
+	channel = CHAN_WEAPON,
+	volume = 0.55,
+	level = 105,
+	pitch = 100,
+	sound = "weapons_nh/hatchet/stab2.wav"
+} )
+
+-------------------------------------------------------
+sound.Add( {
+	name = "NH2.Hatchet_Slash1",
+	channel = CHAN_WEAPON,
+	volume = 0.55,
+	level = 105,
+	pitch = 100,
+	sound = "weapons_nh/hatchet/slash1.wav"
+} )
+
+sound.Add( {
+	name = "NH2.Hatchet_Slash2",
+	channel = CHAN_WEAPON,
+	volume = 0.55,
+	level = 105,
+	pitch = 100,
+	sound = "weapons_nh/hatchet/slash2.wav"
+} )
+
+-------------------------------------------------------
+sound.Add( {
+	name = "NH2.Hatchet_Draw",
+	channel = CHAN_WEAPON,
+	volume = 0.55,
+	level = 105,
+	pitch = 100,
+	sound = "weapons_nh/hatchet/draw.wav"
+} )
