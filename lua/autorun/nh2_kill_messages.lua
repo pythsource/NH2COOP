256439c96d6bc32010b398f35b1bebcfd76f8cd3
Den Urakolouy <41019056+URAKOLOUY5@users.noreply.github.com>
2020-11-01 17:38:02 +0300

    Remove gmod's files

--- lua/autorun/nh2_kill_messages.lua
+++
@@ -1,3 +0,0 @@
-if CLIENT then
-language.Add("npc_nh_cook", "Cook Zombie")
-end
--- lua/autorun/nh2_sounds.lua
+++
@@ -1,8 +0,0 @@
-sound.Add( {
-	name = "NH2Intro",
-	channel = CHAN_VOICE,
-	volume = 1,
-	level = 0,
-	pitch = 100,
-	sound = "nh2/Nh2intro.wav"
-} )
